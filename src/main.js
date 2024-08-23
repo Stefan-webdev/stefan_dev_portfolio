382b1d6ebe49daedd5c3af3b857f1e6fa453b49a
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-08-23 18:39:14 +0930

    run build scripts for preview

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import '../styles/modern-normalise.css';
 import '../styles/style.css';
 import '../styles/components/header.css'; 
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
 import '../styles/utils.css';
